e5a6b2b5fb00531f34a5548fb55787c7d7cd1c8e
Asswad Sarker Nomaan <32814117+asnbd@users.noreply.github.com>
2020-11-20 23:51:19 +0600

    Add LightOffTileService

---
+++ app/src/main/java/me/asswad/myyeelightlan/LightOffTileService.java
@@ -0,0 +1,32 @@
+package me.asswad.myyeelightlan;
+
+import android.graphics.drawable.Icon;
+import android.os.Build;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import androidx.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+public class LightOffTileService extends TileService {
+
+    private String TAG = "LightOffTileService";
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onStartListening() {
+        Tile tile = getQsTile();
+        tile.setIcon(Icon.createWithResource(this, R.drawable.ic_launcher_foreground));
+        tile.setLabel("Light Off");
+        tile.setContentDescription("Turn On Yeelight");
+        tile.updateTile();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+
+        new LightControl().turnOff();
+    }
+
+}
