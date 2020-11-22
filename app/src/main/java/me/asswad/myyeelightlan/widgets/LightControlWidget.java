95a54bd6ed062abb90057abc828ebd63471d1a39
Asswad Sarker Nomaan <32814117+asnbd@users.noreply.github.com>
2020-11-22 19:29:18 +0600

    Light Control Widget Button Click Action set

--- app/src/main/java/me/asswad/myyeelightlan/widgets/LightControlWidget.java
+++ app/src/main/java/me/asswad/myyeelightlan/widgets/LightControlWidget.java
@@ -1,24 +1,40 @@
 package me.asswad.myyeelightlan.widgets;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
 
+import me.asswad.myyeelightlan.LightControl;
+import me.asswad.myyeelightlan.MainActivity;
 import me.asswad.myyeelightlan.R;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class LightControlWidget extends AppWidgetProvider {
+    private final String TAG = "LightControlWidget";
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+    private static final String ACTION_UPDATE_CLICK_ON = "action.UPDATE_CLICK_ON";
+    private static final String ACTION_UPDATE_CLICK_OFF = "action.UPDATE_CLICK_OFF";
+    private static final String ACTION_UPDATE_CLICK_TOGGLE = "action.UPDATE_CLICK_TOGGLE";
+    private static final String ACTION_UPDATE_CLICK_BRIGHT = "action.UPDATE_CLICK_BRIGHT";
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
+    private void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.light_control_widget);
 //        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        views.setOnClickPendingIntent(R.id.w_btn_on, getPendingSelfIntent(context, ACTION_UPDATE_CLICK_ON));
+        views.setOnClickPendingIntent(R.id.w_btn_off, getPendingSelfIntent(context, ACTION_UPDATE_CLICK_OFF));
+        views.setOnClickPendingIntent(R.id.w_btn_toggle, getPendingSelfIntent(context, ACTION_UPDATE_CLICK_TOGGLE));
+        views.setOnClickPendingIntent(R.id.w_btn_bright, getPendingSelfIntent(context, ACTION_UPDATE_CLICK_BRIGHT));
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -40,5 +56,39 @@ public class LightControlWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        switch (intent.getAction()){
+            case ACTION_UPDATE_CLICK_ON:
+                Log.d(TAG, "onReceive: ON CLICKED");
+                new LightControl(context).turnOn();
+                break;
+
+            case ACTION_UPDATE_CLICK_OFF:
+                Log.d(TAG, "onReceive: OFF CLICKED");
+                new LightControl(context).turnOff();
+                break;
+
+            case ACTION_UPDATE_CLICK_TOGGLE:
+                Log.d(TAG, "onReceive: TOGGLE CLICKED");
+                new LightControl(context).toggle();
+                break;
+
+            case ACTION_UPDATE_CLICK_BRIGHT:
+                Log.d(TAG, "onReceive: BRIGHT CLICKED");
+                new LightControl(context).bright();
+                break;
+        }
+
+    }
+
+    private PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, getClass()); // An intent directed at the current class (the "self").
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
     }
 }
