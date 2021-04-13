fbf77647ce815b0584d1aa0c10930b59febc0992
Asswad Sarker Nomaan <32814117+asnbd@users.noreply.github.com>
2021-04-14 04:08:48 +0600

    Add HelpActivity with WebView

---
+++ app/src/main/java/me/asswad/myyeelightlan/HelpActivity.java
@@ -0,0 +1,34 @@
+package me.asswad.myyeelightlan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class HelpActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        getSupportActionBar().setTitle("Help");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        webView = findViewById(R.id.helpWebView);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                view.loadUrl(url);
+                return false;
+            }
+        });
+
+        String url = "https://asnbd.github.io/MyYeelightLan/help.html";
+        webView.loadUrl(url);
+    }
+}
